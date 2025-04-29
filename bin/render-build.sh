b3094df40b235c5329cc62113d50054e0eaa7734
mumbereh <siirasonh@gmail.com>
2025-04-29 10:14:40 +0300

    updated render.yaml

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,11 +3,18 @@
 # Exit on error
 set -o errexit
 
+# Ensure RAILS_MASTER_KEY is set (used to decrypt credentials)
+if [ -z "$RAILS_MASTER_KEY" ]; then
+  echo "RAILS_MASTER_KEY is missing. Please set it in Render > Environment."
+  exit 1
+fi
+
+# Install Ruby gems
 bundle install
+
+# Compile assets
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-# If you have a paid instance type, we recommend moving
-# database migrations like this one from the build command
-# to the pre-deploy command:
-bin/rails db:migrate
+# Run DB migrations
+bundle exec rails db:migrate
